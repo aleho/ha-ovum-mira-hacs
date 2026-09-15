0bfcfa2171037deef974c60333913848bc6523a5
Alexander Hofbauer <a.hofbauer@fify.at>
2026-09-15 13:34:02 +0200

    Implement reconfiguration

--- custom_components/ovum_mira/config_flow.py
+++ custom_components/ovum_mira/config_flow.py
@@ -1,4 +1,5 @@
 """Config flow for Ovum Mira integration."""
+# TODO implement support for multiple WPM units?
 
 from __future__ import annotations
 
@@ -36,43 +37,6 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER = vol.Schema(
-    {
-        vol.Required(CONF_HOST): TextSelector(),
-        vol.Required(CONF_PORT, default=502): vol.All(
-            NumberSelector(
-                NumberSelectorConfig(
-                    mode=NumberSelectorMode.BOX,
-                    min=1,
-                    max=65535,
-                )
-            ),
-            vol.Coerce(int),
-        ),
-        vol.Required(CONF_WPM_UNIT_ID, default=DEFAULT_WPM_UNIT_ID): vol.All(
-            NumberSelector(
-                NumberSelectorConfig(
-                    min=DEFAULT_WPM_UNIT_ID,
-                    max=118,
-                    step=1,
-                    mode=NumberSelectorMode.BOX,
-                )
-            ),
-            vol.Coerce(int),
-        ),
-        vol.Required(CONF_LICENSE_LEVEL, default="1"): vol.All(
-            SelectSelector(
-                SelectSelectorConfig(
-                    mode=SelectSelectorMode.DROPDOWN,
-                    translation_key="license_level",
-                    options=["1", "2"],
-                )
-            ),
-            vol.Coerce(int),
-        ),
-    }
-)
-
 
 class OvumConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Ovum Mira."""
@@ -86,7 +50,6 @@ class OvumConfigFlow(ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
 
         if user_input is not None:
-            # TODO implement support for multiple WPM units
             serial_number = await self._async_probe(user_input)
 
             if not serial_number:
@@ -102,7 +65,40 @@ class OvumConfigFlow(ConfigFlow, domain=DOMAIN):
                 )
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER, errors=errors
+            step_id="user",
+            data_schema=self._build_schema(),
+            errors=errors,
+        )
+
+    async def async_step_reconfigure(self, user_input: dict[str, Any] | None = None):
+        entry = self._get_reconfigure_entry()
+        errors: dict[str, str] = {}
+
+        if user_input is not None:
+            serial_number = entry.unique_id
+
+            if not serial_number:
+                errors[CONF_HOST] = "cannot_connect"
+
+            else:
+                await self.async_set_unique_id(serial_number)
+                self._abort_if_unique_id_mismatch()
+
+                return self.async_update_reload_and_abort(
+                    title="Ovum Mira",
+                    entry=entry,
+                    data_updates=user_input,
+                )
+
+        return self.async_show_form(
+            step_id="reconfigure",
+            errors=errors,
+            data_schema=self._build_schema(
+                host=entry.data[CONF_HOST],
+                port=entry.data[CONF_PORT],
+                wpm_unit_id=entry.data[CONF_WPM_UNIT_ID],
+                license_level=entry.data[CONF_LICENSE_LEVEL],
+            ),
         )
 
     async def _async_probe(self, data: dict[str, Any]) -> str | None:
@@ -129,4 +125,48 @@ class OvumConfigFlow(ConfigFlow, domain=DOMAIN):
             serial_number
             if serial_number is not None and len(serial_number) > 0
             else None
+        )
+
+    @staticmethod
+    def _build_schema(
+        host: str = "",
+        port: int = 502,
+        wpm_unit_id: int = DEFAULT_WPM_UNIT_ID,
+        license_level: int = 1,
+    ) -> vol.Schema:
+        return vol.Schema(
+            {
+                vol.Required(CONF_HOST, default=host): TextSelector(),
+                vol.Required(CONF_PORT, default=port): vol.All(
+                    NumberSelector(
+                        NumberSelectorConfig(
+                            mode=NumberSelectorMode.BOX,
+                            min=1,
+                            max=65535,
+                        )
+                    ),
+                    vol.Coerce(int),
+                ),
+                vol.Required(CONF_WPM_UNIT_ID, default=wpm_unit_id): vol.All(
+                    NumberSelector(
+                        NumberSelectorConfig(
+                            min=DEFAULT_WPM_UNIT_ID,
+                            max=118,
+                            step=1,
+                            mode=NumberSelectorMode.BOX,
+                        )
+                    ),
+                    vol.Coerce(int),
+                ),
+                vol.Required(CONF_LICENSE_LEVEL, default=str(license_level)): vol.All(
+                    SelectSelector(
+                        SelectSelectorConfig(
+                            mode=SelectSelectorMode.DROPDOWN,
+                            translation_key="license_level",
+                            options=["1", "2"],
+                        )
+                    ),
+                    vol.Coerce(int),
+                ),
+            }
         )
